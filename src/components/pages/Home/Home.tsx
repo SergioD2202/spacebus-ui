ebfcb57bd4a7c02eb7a7d695bbc74d139f5aeae4
SergioD2202 <sergioalejandrods1609@gmail.com>
2022-07-31 01:09:57 -0400

    other image

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -2,12 +2,34 @@
 import React, { FC } from 'react';
 import styles from './Home.module.scss';
 import mainImage from '../../../images/saturn.webp';
+import exoplanets from '../../../images/exoplanets.jpg';
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => (
   <div className={styles.Home} data-testid="Home">
-    <img src={mainImage} alt="main image.webp" className={styles.mainImage} />
+    <div className={styles.imageContainer}>
+      <img src={mainImage} alt="main image.webp" className={styles.mainImage} />
+      <div className={styles.titleContainer}>
+        <div className={styles.title}>
+          Conoce lo que el universo tiene para ti
+        </div>
+        <div className={styles.subtitle}>
+          Con Spacebus, viaja a los planetas más lejanos de la vasta vía láctea
+          y mucho más.
+        </div>
+      </div>
+    </div>
+
+    <div className={styles.imageContainer}>
+      <img src={exoplanets} alt="exoplanets.jpg" className={styles.mainImage} />
+      <div className={styles.titleContainer}>
+        <div className={styles.title}>Mira nuestra selección</div>
+        <div className={styles.subtitle}>
+          Tenemos una amplia gama de tickets para escoger.
+        </div>
+      </div>
+    </div>
   </div>
 );
 
