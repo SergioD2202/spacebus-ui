d4bd14522d334b90388cb759d31e6ce8d10354ef
Eduardo <ejuliao9@gmail.com>
2022-08-08 23:31:11 -0400

    la cosa horrorosa

--- src/components/shared/Auth/Auth.tsx
+++ src/components/shared/Auth/Auth.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from 'react';
 import styles from './Auth.module.scss';
+import jupiter from '../../../images/jupiter.jpg';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface AuthProps {}
 
 const Auth: FC<AuthProps> = () => (
   <div className={styles.Auth} data-testid="Auth">
-    Auth Component
+    <div className={styles.imageContainer}>
+      <img src={jupiter} alt="jupiter.jpg" className={styles.mainImage}/>
+      <div className={styles.titleContainer}>
+        <div className={styles.title}>Login</div>
+        <div className={styles.subtitle}>Usuario</div>
+        <div className={styles.subtitle2}>Contrase&ntilde;a.</div>
+      </div>
+    </div>
   </div>
 );
 
