252d7ed32843d8f99fef99b057b86b9883e91b6e
Eduardo <ejuliao9@gmail.com>
2022-08-10 18:09:13 -0400

    Flexbox

--- src/components/shared/Auth/Auth.tsx
+++ src/components/shared/Auth/Auth.tsx
@@ -6,20 +6,43 @@ import jupiter from '../../../images/jupiter.jpg';
 interface AuthProps {}
 
 const Auth: FC<AuthProps> = () => (
-  <div className={styles.Auth} data-testid="Auth">
-    <div className={styles.imageContainer}>
-      <img
-        src={jupiter}
-        alt="jupiter.jpg"
-        className={styles.mainImage + ' d-none d-lg-block'}
-        loading="lazy"
-      />
-      <div className={styles.loginContainer}>
-        <div className={styles.login}>Login</div>
-      </div>
-      <div className={styles.titleContainer}>
-        <div className={styles.user}>Usuario</div>
-        <div className={styles.password}>Contrase&ntilde;a</div>
+  <div className={styles.Auth + ' mx-auto'} data-testid="Auth">
+    <div className="d-none d-lg-flex justify-content-between">
+      <div className="w-100">
+        <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center flex-column">
+            <div className={styles.imageContainer + ' d-none d-lg-flex'}>
+              <img
+                src={jupiter}
+                alt="jupiter.jpg"
+                className={styles.mainImage}
+                loading="lazy"
+              />
+            </div>
+          </div>
+          <div className="d-flex align-items-center flex-column">
+            <div className={styles.loginContainer}>Login</div>
+            <div className={styles.titleContainer}>
+              <form>
+                <label className={styles.user} htmlFor="user">
+                  Usuario
+                </label>
+                <br />
+                <input type="text" id="user" name="user" />
+                <br />
+                <label className={styles.password} htmlFor="pwd">
+                  Contrase&ntilde;a
+                </label>
+                <br />
+                <input type="password" id="pwd" name="pwd" />
+                <br />
+                <button className={styles.button} type="submit">
+                  Entrar
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
