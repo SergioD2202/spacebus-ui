04e41629aec0136165c28bed0e77596cb7d5cb60
SergioD2202 <sergioalejandrods1609@gmail.com>
2022-08-10 21:03:16 -0400

    purchased tickets

--- src/components/shared/Ticket/Ticket.tsx
+++ src/components/shared/Ticket/Ticket.tsx
@@ -8,11 +8,42 @@ interface TicketProps {
   launchDate?: string | Date;
   companyName?: string;
   companyLogo?: string;
+  isPurchaseAvailable?: boolean;
 }
 
 const Ticket: FC<TicketProps> = (props) => (
-  <div className={styles.Ticket + ' shadow-sm rounded'} data-testid="Ticket">
-    {props.description}
+  <div
+    className={
+      styles.Ticket +
+      ' shadow rounded p-2 border border-primary d-flex justify-content-between align-items-center'
+    }
+    data-testid="Ticket"
+  >
+    <div className="d-flex flex-column align-items-center">
+      <img src={props.companyLogo} alt="logo" loading="lazy" />
+      <span className={styles.companyName}>{props.companyName}</span>
+      <span>{props.description}</span>
+    </div>
+
+    <div>
+      Fecha de salida:{' '}
+      {props.launchDate
+        ?.toLocaleString('en-US')
+        .split('T')[0]
+        .split('-')
+        .reverse()
+        .join('-')}
+    </div>
+
+    <h1>{props.price}$</h1>
+
+    {props.isPurchaseAvailable ? (
+      <div>
+        <button className="p-2 btn btn-outline-success">Comprar</button>
+      </div>
+    ) : (
+      <></>
+    )}
   </div>
 );
 
--- src/components/shared/TicketList/TicketList.tsx
+++ src/components/shared/TicketList/TicketList.tsx
@@ -5,12 +5,18 @@ import styles from './TicketList.module.scss';
 interface TicketListProps {
   ticketArray?: any | undefined;
 }
-
 const TicketList: FC<TicketListProps> = (props) => (
   <div className={styles.TicketList + ' w-100'} data-testid="TicketList">
     {props.ticketArray.map((el: any, index: number) => (
       <div className="mb-5" key={index}>
-        <Ticket description={el.description} />
+        <Ticket
+          description={el.description}
+          price={el.price}
+          launchDate={el.rocketLaunche.launchDate}
+          companyName={el.rocketLaunche.spacecraftCompany.name}
+          companyLogo={el.rocketLaunche.spacecraftCompany.logo}
+          isPurchaseAvailable={false}
+        />
       </div>
     ))}
   </div>
